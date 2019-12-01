71be56d45f7e3663af03e697e9126b773ed730d8
chaim1 <cohen1ch@gmail.com>
2019-12-01 23:35:39 +0200

    update

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -8,12 +8,52 @@ import { MainServiceService } from '../servics/main-service.service';
 })
 export class MainComponent implements OnInit {
 contectMain;
+numSliderImage;
+sliderImage = [
+  {
+    img: "../../assets/imagesMain/Retail2.jpg",
+    content:"Applying deep knowledge and experience in handling the retail and commercial sectors’.",
+    header:"RETAIL",
+    id:0
+  },
+ {
+    img: "../../assets/imagesMain/MATRIX_WEBSITE_Liron-brimer1.jpg",
+    content:"Applying deep knowledge and experience in handling the retail and commercial sectors’.",
+    header:"DEFENSE",
+    id:1
+  },
+  {
+    img: "../../assets/imagesMain/GAME-CHANGERS-03.jpg",
+    content:"Applying deep knowledge and experience in handling the retail and commercial sectors’ ",
+    header: 'FINANCE',
+    id:2
+  }
+];
   constructor(private serviceMain: MainServiceService) {
     this.contectMain = serviceMain.contectMain;
-    console.log(this.contectMain);
+    this.numSliderImage = 0;
   }
 
   ngOnInit() {
+  }
+  nextSliderImage(id) {
+      this.numSliderImage = id;
+  }
+  idSliderRev(id){
+    if(this.numSliderImage == 2){
+      this.numSliderImage = 1;
+    }else if(this.numSliderImage == 0) {
+      this.numSliderImage = 2;
+    }else{
+      this.numSliderImage--
+    }
+  }
+  idSliderDrv(id){
+    if(this.numSliderImage == 2){
+      this.numSliderImage = 0;
+    }else{
+      this.numSliderImage++;
+    }
   }
 
 }
